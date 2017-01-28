7994e2a0fed27d60552494ea3ac2eff49ee94aa3
OlivierSM <olivier.morissette@gmail.com>
2017-01-28 16:37:34 -0500

    Refactored and put models in a separate file

--- app.py
+++ app.py
@@ -1,76 +1,23 @@
 from flask import Flask, request
-from sqlalchemy import create_engine, Column,Integer,String
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.types import PickleType
-from sqlalchemy.schema import ForeignKey
+from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy_utils import URLType
+from models import User, Classifiers, Listing, ListingImage, ListingMappedImages, UserVisitedListings, Base
 import json
 import os
-import pickle
 
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] =  'mysql+pymysql://admin:M%m65=N3s-A&ZR3t@mchacks2017.c5se38qdaeio.us-east-1.rds.amazonaws.com:3306/mchacks'
 
-db = create_engine(app.config["SQLALCHEMY_DATABASE_URI"])
 
+# Creating Database
+db = create_engine(app.config["SQLALCHEMY_DATABASE_URI"])
+Base.metadata.create_all(db)
 
 # Used to create database session
 Session = sessionmaker()
 Session.configure(bind=db)
 
-# Creating the Models
-Base = declarative_base()
-
-class User(Base):
-    __tablename__ = 'users'
-    id = Column(Integer,primary_key=True)
-    session_id = Column(String(300),nullable=False) # 300 chosen randomly.
-
-class Classifiers(Base):
-    __tablename__ = "classifiers"
-    id = Column(Integer,primary_key=True)
-    user_id = Column('user_id',Integer,ForeignKey("users.id"),nullable=False)
-    pickled_classifier = Column(PickleType,nullable=False)
-
-class Listing(Base):
-    __tablename__="listing"
-    id = Column(Integer,primary_key=True)
-    title = Column(String(300),nullable=False)
-    location = Column(String(300),nullable=False)
-    city = Column(String(300),nullable=False)
-    description = Column(String(1000),nullable=False)
-    number_rooms = Column(Integer,nullable=False)
-    star_rating = Column(Integer,nullable=False)
-    price = Column(Integer,nullable=False)
-    property_type = Column(String(300),nullable=False)
-
-
-class ListingImage(Base):
-    __tablename__="listing_image"
-    id = Column(Integer,primary_key=True)
-    url = Column(URLType(300),nullable=False)
-
-
-class ListingMappedImages(Base):
-    '''
-    Use this to join images to their corresponding listing image
-    '''
-    __tablename__ = "listing_mapped_images"
-    id = Column(Integer,primary_key=True)
-    listing = Column(Integer,ForeignKey("listing.id"),nullable=False)
-    listing_image = Column(Integer,ForeignKey("listing_image.id"),nullable=False)
-
-
-class UserVisitedListings(Base):
-    __tablename__="user_visited_listings"
-    id = Column(Integer,primary_key=True)
-    listing = Column(Integer,ForeignKey("listing.id"),nullable=False)
-    user_id = Column('user_id',Integer,ForeignKey("users.id"),nullable=False)
-
-# Creates the models
-Base.metadata.create_all(db)
 
 @app.before_request
 def check_id():

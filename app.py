ae29383eed06edec01dd359d891628e4759e266b
Thomas Karatzas <thomas.karatzas@mail.mcgill.ca>
2017-01-28 15:14:56 -0500

    im just hoping at this point

--- app.py
+++ app.py
@@ -12,10 +12,17 @@ db = create_engine(app.config["SQLALCHEMY_DATABASE_URI"])
 @app.route('/', methods =["POST"] )
 def get_con():
     if request.method == "POST":
-        return json.dumps({
-"speech": "Barack Hussein Obama II is the 44th and current President of the United States.",
-"displayText": "Barack Hussein Obama II is the 44th and current President of the United States, and the first African American to hold the office. Born in Honolulu, Hawaii, Obama is a graduate of Columbia University   and Harvard Law School, where ",
-})
+        json_text = json.dumps({
+        "speech": "Barack Hussein Obama II is the 44th and current President of the United States.",
+        "displayText": "Barack Hussein Obama II is the 44th and current President of the United States, and the first African American to hold the office. Born in Honolulu, Hawaii, Obama is a graduate of Columbia University   and Harvard Law School, where ",
+        })
+        
+        return json_text
+
+@app.after_request
+def header(response):
+    response.headers['Content-type'] = ' application/json'
+    return response 
     
     
 if __name__ == "__main__":

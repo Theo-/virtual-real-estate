75cadef68ccc58c2b3c4ddf2e38531626ef71ce5
OlivierSM <olivier.morissette@gmail.com>
2017-01-28 16:23:41 -0500

    Added orm tables for listings

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.types import PickleType
 from sqlalchemy.schema import ForeignKey
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy_utils import URLType
 import json
 import os
 import pickle
@@ -33,19 +34,40 @@ class Classifiers(Base):
     user_id = Column('user_id',Integer,ForeignKey("users.id"),nullable=False)
     pickled_classifier = Column(PickleType,nullable=False)
 
+class Listing(Base):
+    __tablename__="listing"
+    id = Column(Integer,primary_key=True)
+    title = Column(String(300),nullable=False)
+    location = Column(String(300),nullable=False)
+    city = Column(String(300),nullable=False)
+    description = Column(String(1000),nullable=False)
+    number_rooms = Column(Integer,nullable=False)
+    star_rating = Column(Integer,nullable=False)
+    price = Column(Integer,nullable=False)
+    property_type = Column(String(300),nullable=False)
 
 
+class ListingImage(Base):
+    __tablename__="listing_image"
+    id = Column(Integer,primary_key=True)
+    url = Column(URLType(300),nullable=False)
 
-# Description
-# Title
-# Location
-# City
-# Array of Image URLS
-# Rating
-# Number of rooms
-# Price
-# Property Type
 
+class ListingMappedImages(Base):
+    '''
+    Use this to join images to their corresponding listing image
+    '''
+    __tablename__ = "listing_mapped_images"
+    id = Column(Integer,primary_key=True)
+    listing = Column(Integer,ForeignKey("listing.id"),nullable=False)
+    listing_image = Column(Integer,ForeignKey("listing_image.id"),nullable=False)
+
+
+class UserVisitedListings(Base):
+    __tablename__="user_visited_listings"
+    id = Column(Integer,primary_key=True)
+    listing = Column(Integer,ForeignKey("listing.id"),nullable=False)
+    user_id = Column('user_id',Integer,ForeignKey("users.id"),nullable=False)
 
 # Creates the models
 Base.metadata.create_all(db)

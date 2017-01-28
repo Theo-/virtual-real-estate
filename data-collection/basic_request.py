533e595654731b07add6e4d60c9a7856e4487f65
arthur Gaubil <arthurgaubil@arthurs-MacBook-Pro.local>
2017-01-28 13:21:38 -0500

    finished get_review function

--- data-collection/basic_request.py
+++ data-collection/basic_request.py
@@ -36,8 +36,24 @@ def get_airbnb_listing(client_id,**kwargs):
     pass
 
 
-def get_airbnb_review(client_id,**kwargs):
-    pass
+def get_airbnb_review(client_id, **kwargs):
+
+    '''
+    This requires :
+        client_id:	3092nxybyb0otqw18e8nh5nty //This works for the moment
+        role:	all  //Not sure, but it's required.
+        listing_id	2056659 //ID of the listing you'd like to view reviews for.
+    Optional :
+        locale	en-US	Desired lagnuage
+        currency USD	Desired currency
+        _format	for_mobile_client || for_search_results || for_search_results_with_minimal_pricing	Not sure what the difference is.
+        _limit	10	Number of reviews to show at a time.
+        _offset	0	Number of reviews to offset.
+    '''
+
+    url = "https://api.airbnb.com/v2/reviews?role=all&client_id="+client_id
+    r = requests.get(url,params=kwargs)
+    return r
 
 
 def get_airbnb_hosts(client_id,**kwargs):
@@ -46,7 +62,6 @@ def get_airbnb_hosts(client_id,**kwargs):
 
 def get_airbnb_listing_info(client_id,**kwargs):
     pass
-
 
 #r = requests.get("https://api.airbnb.com/v2/search_results",params=url3)
 #print(r.text)

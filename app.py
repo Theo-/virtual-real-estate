26b1e70a937b2729af3170e63cb3db06f79ddb82
Thomas Karatzas <thomas.karatzas@mail.mcgill.ca>
2017-01-28 17:56:26 -0500

    added proper middleware and create user

--- app.py
+++ app.py
@@ -21,15 +21,15 @@ Session.configure(bind=db)
 
 @app.before_request
 def check_id():
+    print request
     if request.method == 'POST':
         sess_id = request.args['sessionId']
         session = Session()
-        var = session.query(User).filter_by(session_id = sess_id).all()
-        if len(var) == 0:
-            user = User(session_id=sess_id)
-            session.add(user)
-            session.commit()
-
+        user = session.query(User).filter_by(session_id = sess_id).all()
+        if len(user) == 0:
+            create_new_user(sess_id)
+        else:
+            gauss_clf = session.query(Classifiers).filter_by(user_id=user[0].id).first()
 
 @app.route('/testdatabase', methods = ["POST"] )
 def testdatabase():
@@ -74,6 +74,16 @@ def get_con():
 def header(response):
     response.headers['Content-type'] = ' application/json'
     return response
+
+def create_new_user(sess_id):
+    session = Session() # add a new user
+    gauss_clf = GaussianNB()
+    user = User(session_id=sess_id)
+    session.add(user)
+    user_id = session.query(User).filter_by(session_id = sess_id).all()[0].id
+    classifier = Classifiers(user_id=user_id,pickled_classifier=gauss_clf)
+    session.add(classifier)
+    session.commit()
 
 if __name__ == "__main__":
     app.run(host = "0.0.0.0", port=int(os.environ['PORT']),threaded=True,debug=True)

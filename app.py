43b557c85efbc4f0bbfa4eb5ab503837607cfd77
Thomas Karatzas <thomas.karatzas@mail.mcgill.ca>
2017-01-28 15:34:45 -0500

    added middle ware

--- app.py
+++ app.py
@@ -9,13 +9,15 @@ app.config['SQLALCHEMY_DATABASE_URI'] =  'mysql://admin:M%m65=N3s-A&ZR3t@mchacks
 
 db = create_engine(app.config["SQLALCHEMY_DATABASE_URI"])
 
+@app.before_request
+def check_id():
+    if request.method == 'POST':
+        return "hello"
+    
 @app.route('/', methods =["POST"] )
 def get_con():
     if request.method == "POST":
-        json_text = json.dumps({
-        "speech": "Barack Hussein Obama II is the 44th and current President of the United States.",
-        "displayText": "Barack Hussein Obama II is the 44th and current President of the United States, and the first African American to hold the office. Born in Honolulu, Hawaii, Obama is a graduate of Columbia University   and Harvard Law School, where ",
-        })
+        json_text = json.dumps({"id": request.args['sessionId']})
         
         return json_text
 

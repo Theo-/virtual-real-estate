9bc8b690195f57e9a689a0284dc6dc3fd12e1eba
Thomas Karatzas <thomas.karatzas@mail.mcgill.ca>
2017-01-28 14:32:15 -0500

    fixed typo

--- app.py
+++ app.py
@@ -8,10 +8,10 @@ app = Flask(__name__)
 @app.route('/', methods =["POST"] )
 def get_con():
     if request.method == "POST":
-        return json.dumps({{
+        return json.dumps({
 "speech": "Barack Hussein Obama II is the 44th and current President of the United States.",
 "displayText": "Barack Hussein Obama II is the 44th and current President of the United States, and the first African American to hold the office. Born in Honolulu, Hawaii, Obama is a graduate of Columbia University   and Harvard Law School, where ",
-}})
+})
     
 
 if __name__ == "__main__":
